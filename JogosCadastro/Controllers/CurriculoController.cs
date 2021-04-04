007e7c0d68eaaac3feb4bab154b87b3df84c7c11
RodrigoEmiliano27 <54250496+RodrigoEmiliano27@users.noreply.github.com>
2021-04-04 16:58:02 -0300

    Retirada validações no backend

--- JogosCadastro/Controllers/CurriculoController.cs
+++ JogosCadastro/Controllers/CurriculoController.cs
@@ -69,58 +69,53 @@ namespace TrabalhoCurriculo.Controllers
             {
 
                 ViewBag.Idioma = getSelectedLanguage();
-                ValidaDados(cur);
-                if (!ModelState.IsValid)
+                CurriculoDAO dao = new CurriculoDAO();
+                FormacaoDAO fdao = new FormacaoDAO();
+                IdiomaDAO Idao = new IdiomaDAO();
+                HabilidadesDAO Hdao = new HabilidadesDAO();
+                //cur.Nascimento = Convert.ToDateTime("10/07/1997");
+                cur.ImagemEmByte = ConvertImageToByte(cur.Imagem);
+                if (dao.Consulta(cur.Id) == null)
                 {
-                    return View("Form", cur);
+                    id = dao.ProximoId();
+                    dao.Inserir(cur);
+
+                    foreach (FormacaoViewModel f in cur.Formacao)
+                    {
+                        f.IdCurriculo = id;
+                        fdao.Inserir(f);
+                    }
+
+                    foreach (IdiomaViewModel d in cur.Idiomas)
+                    {
+                        d.IdCurriculo = id;
+                        Idao.Inserir(d);
+                    }
+
+                    foreach (HabilidadesViewModel h in cur.Habilidades)
+                    {
+                        h.IdCurriculo = id;
+                        Hdao.Inserir(h);
+                    }
+
                 }
                 else
                 {
-                    CurriculoDAO dao = new CurriculoDAO();
-                    FormacaoDAO fdao = new FormacaoDAO();
-                    IdiomaDAO Idao = new IdiomaDAO();
-                    HabilidadesDAO Hdao = new HabilidadesDAO();
-                    //cur.Nascimento = Convert.ToDateTime("10/07/1997");
-                    cur.ImagemEmByte = ConvertImageToByte(cur.Imagem);
-                    if (dao.Consulta(cur.Id) == null)
+                    CompareCurriculos Compare = new CompareCurriculos(dao.Consulta(cur.Id), cur);
+                    Compare.CompararCurriculo();
+                    if (cur.StatusImg == "Editar")
                     {
-                        id = dao.ProximoId();
-                        dao.Inserir(cur);
 
-                        foreach (FormacaoViewModel f in cur.Formacao)
-                        {
-                            f.IdCurriculo = id;
-                            fdao.Inserir(f);
-                        }
-
-                        foreach (IdiomaViewModel d in cur.Idiomas)
-                        {
-                            d.IdCurriculo = id;
-                            Idao.Inserir(d);
-                        }
-
-                        foreach (HabilidadesViewModel h in cur.Habilidades)
-                        {
-                            h.IdCurriculo = id;
-                            Hdao.Inserir(h);
-                        }
-
+                        dao.AlterarImagem(cur.ImagemEmByte, cur.Id);
                     }
-                    else
-                    {
-                        CompareCurriculos Compare = new CompareCurriculos(dao.Consulta(cur.Id), cur);
-                        Compare.CompararCurriculo();
-                        if (cur.StatusImg == "Editar")
-                        {
-
-                            dao.AlterarImagem(cur.ImagemEmByte, cur.Id);
-                        }
-                    }
-
-                    return RedirectToAction("index");
                 }
-                
-                
+
+                return RedirectToAction("index");
+
+
+
+
+
 
             }
             catch (Exception erro)
@@ -129,48 +124,7 @@ namespace TrabalhoCurriculo.Controllers
             }
 
         }
-        private void ValidaDados(CurriculoViewModel cur)
-        {
-            TextosValidacoes validacoes = new TextosValidacoes(ViewBag.Idioma);
-
-
-            if (string.IsNullOrEmpty(cur.Nome))
-                ModelState.AddModelError("Nome", validacoes.Nome_vazio);
-            if (cur.Nascimento > DateTime.Now)
-                ModelState.AddModelError("Nascimento", validacoes.Nascimento_invalido);
-            if (string.IsNullOrEmpty(cur.Telefone))
-                ModelState.AddModelError("Telefone", validacoes.Telefone_vazio);
-            if (string.IsNullOrEmpty(cur.CPF))
-                ModelState.AddModelError("CPF", validacoes.Cpf_vazio);
-            if (string.IsNullOrEmpty(cur.Email))
-                ModelState.AddModelError("Email", validacoes.Email_vazio);
-            if (string.IsNullOrEmpty(cur.Cargo_Pretendido))
-                ModelState.AddModelError("Cargo_Pretendido",validacoes.Cargo_vazio);
-            if (string.IsNullOrEmpty(cur.Rua))
-                ModelState.AddModelError("Rua", validacoes.Rua_vazio);
-            if (cur.Numero_Endereco==null || cur.Numero_Endereco<0)
-                ModelState.AddModelError("Numero_Endereco", validacoes.Numero_invalido);
-            if (string.IsNullOrEmpty(cur.CEP))
-                ModelState.AddModelError("CEP", validacoes.Cep_vazio);
-            if (string.IsNullOrEmpty(cur.Bairro))
-                ModelState.AddModelError("Bairro", validacoes.Bairro_vazio);
-            if (string.IsNullOrEmpty(cur.Estado))
-                ModelState.AddModelError("Estado", validacoes.Estado_vazio);
-
-            validaFormacoes(cur, validacoes);
-
-
-        }
-        private void validaFormacoes(CurriculoViewModel cur, TextosValidacoes text)
-        {
-            for (int n = 0; n < cur.Formacao.Count; n++)
-            {
-                if(cur.Formacao[n].Instituicao=="" || cur.Formacao[n].Instituicao ==null)
-                    ModelState.AddModelError($"Formacao[{n}].Instituicao", text.Formacao_intituicao_vazio);
-                if (cur.Formacao[n].Descricao == ""|| cur.Formacao[n].Descricao ==null)
-                    ModelState.AddModelError($"Formacao[{n}].Descricao", text.Formacao_Descricao_vazio);
-            }
-        }
+        
         /// <summary>
         /// Converte a imagem recebida no form em um vetor de bytes
         /// </summary>
